31314d86d023dbc067b30260e9ba1f299b6796a0
Klirik <dasoftweb@gmail.com>
2021-04-15 22:29:26 +0300

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -6,6 +6,7 @@ const API_KEY = 'be2bb7fd29eddf6e05cfa10ca2e7b19c';
 export default class MovieApiService {
   constructor() {
     this.searchQuery = '';
+    this.genre = '';
     this.page = 1;
   }
 
@@ -31,10 +32,22 @@ export default class MovieApiService {
       .catch(error => console.log(error));
   }
 
+  //Возвращаем поисковый запрос по жанру
+
+  async fetchSearchGenres() {
+    const url = `${BASE_URL}/discover/movie?api_key=${API_KEY}&page=${this.page}&with_genres=${this.genre}`;
+
+    return await fetch(url)
+      .then(response => response.json())
+      .then(response => response.results)
+      .catch(error => console.log(error));
+  }
+
   //Возвращаем списока жанров
 
   fetchGenres() {
     const url = `${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`;
+    console.log(url);
 
     return fetch(url)
       .then(response => response.json())
@@ -79,9 +92,24 @@ export default class MovieApiService {
     return this.normalizeMoviesList(this.fetchPopularMovies());
   }
 
+  // genres.filter(genre => genre.name === genrename);
+  async getMoviesByGenre(genrename) {
+    let genreId = await this.fetchGenres().then(genres => genres.filter(genre => genre.name === genrename).map(obj => obj.id));
+    this.genre = genreId;
+    return this.normalizeMoviesList(this.fetchSearchGenres());
+  }
+
   increamentPage() {
     this.page += 1;
   }
+
+  // get genre() {
+  //   return this.genre;
+  // }
+
+  // set genre(newGenre) {
+  //   this.genre = newGenre;
+  // }
 
   resetPage() {
     this.page = 1;
@@ -93,7 +121,10 @@ const movieApiServie = new MovieApiService();
 const galleryRef = document.querySelector('.gallery-list');
 
 //Вызов Search
-movieApiServie.getMovie('deep');
+// movieApiServie.getMovie('doom');
 
 //Вызов Popular
-movieApiServie.getPopularMovies();
+// movieApiServie.getPopularMovies();
+
+//Вызов Search by genre
+movieApiServie.getMoviesByGenre('Horror');
